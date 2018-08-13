0b53b10b2d8a74bc04441b0af82239214d00ca8b
Guilherme Miguel Teixeira Rito <guilhermemtr@gmail.com>
2018-08-13 20:37:28 +0100

    Slightly changed the implementation of the homomorpshim

--- cpp/include/free_monoid/free_monoid_homomorphism.hpp
+++ cpp/include/free_monoid/free_monoid_homomorphism.hpp
@@ -1,7 +1,6 @@
 #ifndef __PLACID_FREE_MONOID_HOMOMORPHISM__
 #define __PLACID_FREE_MONOID_HOMOMORPHISM__
 
-#include "magma_element.hpp"
 #include "free_monoid_element.hpp"
 #include "tuple.hpp"
 
@@ -14,13 +13,13 @@ namespace __placid
     {
       // the element with index 0 corresponds to the identity for type T.
       T *    mapping;
-      symbol rank;
+      size_t length;
 
       homomorphism (const T *elems, const symbol rank)
       {
-        this->rank    = rank;
+        this->length  = rank + 1;
         this->mapping = new T[this->rank];
-        for (symbol s = 0; s < rank; s++)
+        for (symbol s = 0; s < this->length; s++)
         {
           this->mapping[s] = elems[s];
         }
@@ -31,12 +30,26 @@ namespace __placid
       {
       }
 
+      symbol
+      max_rank() const
+      {
+        return this->length - 1;
+      }
+
       T
-      get_val (const element &e) const
+      map (const element &e) const
       {
         if (e.get_size () == 0)
         {
           return mapping[0];
+        }
+
+        for (size_t i = 0; i < e.length; i++)
+        {
+          if (e.word[i].sym > this->max_rank())
+          {
+            throw std::string ("word with invalid rank");
+          }
         }
 
         T res = this->mapping[e.word[0].sym];
@@ -57,6 +70,17 @@ namespace __placid
         return res;
       }
     };
+
+    /*    template <typename T>
+    check_homomorphism_injectivity (homomorphism &h, pool<B> &o)
+    {
+      pool<T> =
+      for (size_t i = 0; i < o.counter; i++)
+      {
+        this->add (h.map (o.elements[i]));
+      }
+      }*/
+
 
   }    // namespace free_monoid
 
