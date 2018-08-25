528b9ca500e84717dc348cd0b3380c54ce1065e9
Guilherme Miguel Teixeira Rito <guilhermemtr@gmail.com>
2018-08-25 12:03:00 +0100

    Added more documentation. Documenting the free monoid stuff.

--- cpp/include/free_monoid/factor_monoid_element.hpp
+++ cpp/include/free_monoid/factor_monoid_element.hpp
@@ -17,16 +17,30 @@ namespace __placid
   namespace free_monoid
   {
     struct element;
-    
+
     template <typename T>
     struct factor_element : public magma_element<T>
     {
+      /** Assigns the factor element the given word.
+       * Assigns the factor element the given word.
+       * @param [in] w - the word.
+       * @return the new value of the factor element.
+       */
       virtual T &
       operator= (const element &w) = 0;
 
+      /** Gets a reading of the element.
+       * Gets a reading of the element.
+       * @return the reading of the element.
+       */
       virtual element
       reading () const = 0;
 
+      /** Multiplies two factor elements by using the word concatenation.
+       * Multiplies two factor elements by using the word concatenation.
+       * @param [in] o - the second element.
+       * @return the product of the factor elements.
+       */
       T operator* (const T &o) const
       {
         T res = this->reading () * o.reading ();
--- cpp/include/free_monoid/free_monoid_entry.hpp
+++ cpp/include/free_monoid/free_monoid_entry.hpp
@@ -11,29 +11,69 @@ namespace __placid
 {
   namespace free_monoid
   {
-    typedef uint64_t symbol;
-    typedef uint64_t occurrences;
+    typedef uint64_t symbol;    // !< represents the type of a symbol of a
+                                // semistandard tableaux.
+    typedef uint64_t
+      occurrences;    // !< represents the type used to define the number of
+                      // occurrences of a symbol in the tableaux.
 
+    /**
+     * This class defines an entry of a semistandard tableaux.
+     * This class defines an entry of a semistandard tableaux.
+     */
     struct entry
     {
-      symbol      sym;
-      occurrences count;
+      symbol      sym; // !< the symbol of the entry.
+      occurrences count; // !< the number of occurrences of the symbol.
 
+      /** Creates a new entry given a value.
+       * Creates a new entry given a value.
+       * @param [in] sym - the symbol.
+       */
       entry (symbol sym = 0L);
 
+      /** Creates a new entry given a value and the number of occurrences.
+       * Creates a new entry given a value and the number of occurrences.
+       * @param [in] sym - the symbol.
+       * @param [in] count - the number of occurrences.
+       */
       entry (symbol sym, occurrences count);
 
+      /** Creates a new entry given another entry.
+       * Creates a new entry given another entry.
+       * @param [in] t - the other entry.
+       */
       entry (entry &t);
 
+      /** Assigns a new entry to the entry.
+       * Assigns a new entry to the entry.
+       * @param [in] o - the new entry.
+       * @return a reference to the value of the entry.
+       */
       entry
       operator= (entry o);
 
+      /** Assigns a new value to the entry, with count set to 1.
+       * Assigns a new value to the entry, with count set to 1.
+       * @param [in] sym - the new symbol of the entry.
+       * @return a reference to the value of the entry.
+       */
       entry
       operator= (symbol sym);
 
+      /** Checks if two entries are equal.
+       * Checks if two entries are equal.
+       * @param [in] o - the second entry.
+       * @return if the two entries are equal.
+       */
       bool
       operator== (entry e);
 
+      /** Checks if two entries are different.
+       * Checks if two entries are different.
+       * @param [in] o - the second entry.
+       * @return if the two entries are different.
+       */
       bool
       operator!= (entry e);
     };
